8d7ff9dc321171c5a7d8f81b2a36d94e413b91f6
JagnaJuras <116656769+JagnaJuras@users.noreply.github.com>
2022-11-07 23:17:29 +0100

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -6,3 +6,7 @@ class TestCalc(unittest.TestCase):
     def test_div_numbers(self):
         result = calc.div_numbers(10, 5)
         self.assertEqual(result, 2)
+        
+    def test_mul_numbers(self):
+        result = calc.mul_numbers(10, 5)
+        self.assertEqual(result, 50)
