4547ad8cc94292addd7142db429d70be5cd0d174
JagnaJuras <116656769+JagnaJuras@users.noreply.github.com>
2022-11-07 15:03:46 +0100

    Dodano plik calc.py

---
+++ calc.py
@@ -0,0 +1,20 @@
+def div_numbers(a, b):
+    return a / b
+
+
+def mul_numbers(a, b):
+    return a * b
+
+
+def add_numbers(a, b):
+    return a + b
+
+
+def print_msg():
+    print("To jest początek programu.")
+
+
+print_msg()
+print(f'Div numbers of 2 and 4 is equal {div_numbers(2, 4)}')
+print(f'Add numbers of 2 and 4 is equal {add_numbers(2, 4)}')
+print(f'Mul numbers of 2 and 4 is equal {mul_numbers(2, 4)}')
